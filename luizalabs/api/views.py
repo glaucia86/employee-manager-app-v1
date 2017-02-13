6542f6c60d3e7b949c1ffe947f80fc6ed1fd2b57
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-13 16:57:54 -0200

    Desenvolvendo o a queryset para os métodos: GET, PUT & DELETE.

--- luizalabs/api/views.py
+++ luizalabs/api/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 
 
 # Essa classe será responsável por criar o comportamento da nossa api:
-# O ListCreateAPIVIEW retorna os requests: [GET] & [POST]
+# O 'ListCreateAPIVIEW' retorna os requests: [GET] & [POST]
 class CreateView(generics.ListCreateAPIView):
     queryset = EmployeeList.objects.all()
     serializer_class = EmployeeListSerializer
@@ -14,3 +14,9 @@ class CreateView(generics.ListCreateAPIView):
     #Aqui irei salvar os dados via 'post' quando for criar um novo 'employee':
     def perform_create(self, serializer):
         serializer.save()
+
+#Essa classe será responsável por manipular os métodos http: GET, PUT e DELETE:
+# O 'RetrieveUpdateDestroyAPIView' é uma view genérica que provê os métodos: GET (by Id), PUT, PATCH & DELETE
+class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = EmployeeList.objects.all()
+    serializer_class = EmployeeListSerializer
