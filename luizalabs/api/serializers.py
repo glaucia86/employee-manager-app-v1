886add8e74b423d3703cb6370bb244a63769b6ff
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-13 15:41:23 -0200

    Desenvolvimento do arquivo 'serializers.py'

---
+++ luizalabs/api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import EmployeeList
+
+#Aqui estou mapeando a instância da classe modelo para o formato JSON:
+class EmployeeListSerializer(serializers.ModelSerializer):
+
+    #O 'Meta' irá mapear os campos serializados com os campos da classe modelo:
+    class Meta:
+        model = EmployeeList
+        fields = ('id', 'name', 'email', 'departament')
