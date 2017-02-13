9ed68e7f7c0dee47e2c35751d8f7df2d8fc003d6
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-13 17:16:40 -0200

    Alteração na url

--- luizalabs/api/urls.py
+++ luizalabs/api/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import CreateView
+from .views import DetailsView
 
 urlpatterns = {
-    url(r'^employee/$', CreateView.as_view(), name="Add"),
+    url(r'^employee/$', CreateView.as_view(), name="create"),
+    url(r'^employee/(?P<pk>[0-9]+)/$', DetailsView.as_view(), name="details"),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
