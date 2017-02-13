cf455979009d9992fa57d67a6def74cbe476082f
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-13 16:45:43 -0200

    Desenvolvimento da função para test para o Método: DELETE

--- luizalabs/api/tests.py
+++ luizalabs/api/tests.py
@@ -37,7 +37,7 @@ class ViewTestCase(TestCase):
         #employeelist_data = EmployeeListSerializer(instance=self.employeelist).data
         self.employeelist_data = {'name': 'Glau Lemos', 'email': 'skinclear86@gmail.com', 'departament': 'Information Technology'}
         self.response = self.client.post(
-            reverse('Add'),
+            reverse('create'),
             self.employeelist_data,
             format="json")
 
@@ -49,19 +49,31 @@ class ViewTestCase(TestCase):
     def test_api_can_get_a_employeelist(self):
         employeelist = EmployeeList.objects.get()
         response = self.client.get(
-            reverse('Details'),
+            reverse('details'),
             kwargs={'pk': employeelist.id}, format='json')
 
         self.assertAlmostEqual(response.status_code, status.HTTP_200_OK)
         self.assertContains(response, employeelist)
 
-    # Aqui iremos testar se estará atualizando os valores para employee: METHOD: UPDATE
+    # Aqui iremos testar se estará atualizando os valores para employee: METHOD: PUT
     def test_api_can_update_employeelist(self):
         update_employeelist = {'name': 'Updating Employee', 'email': 'update@update.com', 'departament': 'Updating Departament'}
         res = self.client.put(
-            reverse('Update', kwargs{'pk': employeelist.id}),
-            update_employeelist, format='json'
-        )
+            reverse('details', kwargs{'pk': employeelist.id}),
+            update_employeelist, format='json')
+    
+    self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    
+    #Aqui iremos testar se estará deletando o valor pelo Id: METHOD: DELETE:
+    def_test_api_can_delete_employeelist(self):
+        employeelist = EmployeeList.objects.get()
+        response = self.client.delete(
+            reverse('details', kwargs={'pk': employeelist.id}),
+            format='json',
+            follow=True)
+
+    self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
 
 
 
