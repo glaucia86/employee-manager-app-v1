50c1a893c341a0bba9c8d8a1ed22308fe74b134e
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-13 15:15:53 -0200

    Desenvolvimento do modelo da classe 'EmployeeList' no arquivo 'models.py'

--- luizalabs/api/models.py
+++ luizalabs/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+#Definindo a classe: EmployeeList:
+class EmployeeList(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=50)
+    departament = models.CharField(max_length=50)
+
+
+    #Aqui estou criando uma função para que retorne nome do empregado no admin:
+    def __str__(self):
+        return self.name
+    
