11bc61a97d2bc78112b8ba802c70d260dce3c396
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-13 19:22:53 -0200

    Alterações significativas no arquivo tests.py

--- luizalabs/api/tests.py
+++ luizalabs/api/tests.py
@@ -1,7 +1,8 @@
-from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
 from django.core.urlresolvers import reverse
+
+from django.test import TestCase
 from .models import EmployeeList
 
 """Esta classe irá definir o conjunto de testes para o modelo da classe: 'EmployeeList':"""
@@ -20,7 +21,7 @@ class ModelTestCase(TestCase):
         self.assertNotEqual(old_count, new_count)
 
 """ Aqui irei testar a parte lógica da aplicação: 'views.py':"""
-class ViewsTestCase(TestCase):
+class ViewTestCase(TestCase):
 
     def setUp(self):
         self.client = APIClient()
@@ -36,30 +37,32 @@ class ViewsTestCase(TestCase):
     
     """ Aqui iremos testar se está retornando todos os valores dados do employee: METHOD: GET (By Id) """
     def test_api_can_get_a_employeelist(self):
-        employeelist = EmployeeList.objects.get(id=1)
+
+        employeelist = EmployeeList.objects.get()
         response = self.client.get(
-            '/employee/',
+            reverse('details'),
             kwargs={'pk': employeelist.id}, format="json")
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertContains(response, employeelist)
     
-    """# Aqui iremos testar se estará atualizando os valores para employee: METHOD: PUT"""
+    """ Aqui iremos testar se estará atualizando os valores para employee: METHOD: PUT"""
     def test_api_can_update_employeelist(self):
+
         employeelist = EmployeeList.objects.get()
-        change_employeelist = {'name': 'Something new'}
+        change_employeelist = {'name': 'New Employee'}
         res = self.client.put(
             reverse('details', kwargs={'pk': employeelist.id}),
-            change_employeelist, format='json'
-        )
+            change_employeelist, format="json")
+
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
-    """Aqui iremos testar se estará deletando o valor pelo Id: METHOD: DELETE:"""
+    """ Aqui iremos testar se estará deletando o valor pelo Id: METHOD: DELETE: """
     def test_api_can_delete_employeelist(self):
         employeelist = EmployeeList.objects.get()
         response = self.client.delete(
-            reserve('details', kwargs={'pk': employeelist.id}),
-            format='json',
+            reverse('details', kwargs={'pk': employeelist.id}),
+            format="json",
             follow=True)
 
-    self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
