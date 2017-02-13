4fc3763871ea1eed2918093a6be9c92d0581e4a2
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-13 16:12:33 -0200

    Desenvolvimento da views.py

--- luizalabs/api/views.py
+++ luizalabs/api/views.py
@@ -1,3 +1,16 @@
+from rest_framework import generics
+from .serializers import EmployeeListSerializer
+from .models import EmployeeList
+
 from django.shortcuts import render
 
-# Create your views here.
+
+# Essa classe será responsável por criar o comportamento da nossa api:
+# O ListCreateAPIVIEW retorna os requests: [GET] & [POST]
+class CreateView(generics.ListCreateAPIView):
+    queryset = EmployeeList.objects.all()
+    serializer_class = EmployeeListSerializer
+
+    #Aqui irei salvar os dados via 'post' quando for criar um novo 'employee':
+    def perform_create(self, serializer):
+        serializer.save()
