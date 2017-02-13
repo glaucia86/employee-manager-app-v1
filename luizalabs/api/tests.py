7569970a1c72a0b9b81c4c7b866e6b638891fc4c
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-13 15:06:59 -0200

    Desenvolvimento do TDD p/ app no arquivo 'tests.py'

--- luizalabs/api/tests.py
+++ luizalabs/api/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import EmployeeList
 
-# Create your tests here.
+#Esta classe irá definir o conjunto de testes para o modelo da classe: 'Employee':
+class ModelTestCase(TestCase):
+
+    #Essa função aqui será responsável por testar o cliente e as variáveis
+    def setUp(self):
+        self.employeelist_name = "Glaucia Lemos"
+        self.employeelist = EmployeeList(name=self.employeelist_name)
+
+        self.employeelist_email = "glaucia.lemos@luizalabs.com"
+        self.employeelist = EmployeeList(name=self.employeelist_email)
+
+        self.employeelist_departament = "Information Technology"
+        self.employeelist = EmployeeList(name=self.employeelist_departament)
+        
+
+    #Essa função irá testar o modelo da classe:
+    def test_model_can_create_a_employeelist(self):
+        old_count = EmployeeList.objects.count()
+        self.employeelist.save()
+        new_count = EmployeeList.objects.count()
+        self.assertNotEqual(old_count, new_count)
+
+
+
